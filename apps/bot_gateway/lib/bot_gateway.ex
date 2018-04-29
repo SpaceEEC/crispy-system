85e9429c6388950037ee5685a1f3eca3de43ad5e
SpaceEEC <wermail2002-kap@yahoo.de>
2018-04-29 13:12:45 +0200

    fix(Gateway): use bangified fetch_env function

--- apps/bot_gateway/lib/bot_gateway.ex
+++ apps/bot_gateway/lib/bot_gateway.ex
@@ -16,7 +16,7 @@ defmodule Bot.Gateway do
   def voice_state_update(guild_id, channel_id, states \\ []) do
     use Bitwise
 
-    shard_count = Application.fetch_env(:crux_gateway, :shard_count)
+    shard_count = Application.fetch_env!(:crux_gateway, :shard_count)
     shard_id = guild_id >>> 22 |> rem(shard_count)
 
     Gateway.Command.voice_state_update(guild_id, channel_id, states)
