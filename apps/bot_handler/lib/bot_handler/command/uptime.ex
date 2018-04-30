2c2b7e7053672a40ed492337d903886f34f90ebb
SpaceEEC <wermail2002-kap@yahoo.de>
2018-04-30 17:37:42 +0200

    feat(Handler.Command.Uptime): add support for days

--- apps/bot_handler/lib/bot_handler/command/uptime.ex
+++ apps/bot_handler/lib/bot_handler/command/uptime.ex
@@ -27,6 +27,17 @@ defmodule Bot.Handler.Command.Uptime do
       |> to_string
       |> String.pad_leading(2, "0")
 
-    rest(:create_message, [message, [content: "Uptime: #{hours}:#{minutes}:#{seconds}"]])
+    days = div(total, 24)
+
+    uptime =
+      if days == 0 do
+        "Uptime:"
+      else
+        "Uptime: #{days} days"
+      end
+
+    uptime = "#{uptime} #{hours}:#{minutes}:#{seconds}"
+
+    rest(:create_message, [message, [content: uptime]])
   end
 end
