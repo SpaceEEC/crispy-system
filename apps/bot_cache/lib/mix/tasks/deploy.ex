6956feed8ade1e7ccac59999b71d70cef72ef548
SpaceEEC <spaceeec@yahoo.com>
2018-05-28 18:57:26 +0200

    feat: allow deploy to also upgrade

--- apps/bot_cache/lib/mix/tasks/deploy.ex
+++ apps/bot_cache/lib/mix/tasks/deploy.ex
@@ -51,19 +51,32 @@ defmodule Mix.Tasks.Deploy do
     end
 
     def deploy_release(config) do
-      Mix.shell.info "Deploying release to #{config.deploy_dir}"
-      File.mkdir_p!(config.deploy_dir)
+      Mix.shell().info("Deploying release to #{config.deploy_dir}")
 
-      Mix.shell.info "Extracting tarball #{config.tarball}"
-      :ok = :erl_tar.extract(config.tarball, [{:cwd, config.deploy_dir}, :compressed])
+      if config.new do
+        File.mkdir_p!(config.deploy_dir)
+        Mix.shell().info("Extracting tarball #{config.tarball}")
+        :ok = :erl_tar.extract(config.tarball, [{:cwd, config.deploy_dir}, :compressed])
+      else
+        Mix.shell().info("Copying tarball #{config.tarball}")
+        target_dir = "#{config.deploy_dir}/releases/#{config.version}"
+        File.mkdir_p!(target_dir)
+        File.copy!(config.tarball, "#{target_dir}/#{config.app_name}.tar.gz")
+
+        if config.upgrade do
+          Mix.shell().info("Upgrading...")
+          executeable_path = "#{config.deploy_dir}/bin/#{config.app_name}"
+          System.cmd(executeable_path, ["upgrade", config.version])
+        end
+      end
 
       nil
     end
 
     # def create_timestamp do
-      # {{year, month, day}, {hour, minute, second}} = :calendar.now_to_universal_time(:os.timestamp())
-      # timestamp = :io_lib.format("~4..0B~2..0B~2..0B~2..0B~2..0B~2..0B", [year, month, day, hour, minute, second])
-      # timestamp |> List.flatten |> to_string
+    # {{year, month, day}, {hour, minute, second}} = :calendar.now_to_universal_time(:os.timestamp())
+    # timestamp = :io_lib.format("~4..0B~2..0B~2..0B~2..0B~2..0B~2..0B", [year, month, day, hour, minute, second])
+    # timestamp |> List.flatten |> to_string
     # end
 
     # There is a problem the startup scripts if the path includes ":" chars
@@ -77,31 +90,45 @@ defmodule Mix.Tasks.Deploy do
     def parse_args(argv) do
       {args, _, _} = OptionParser.parse(argv)
 
-      app_name = Mix.Project.config[:app] |> Atom.to_string
-      version = Mix.Project.config[:version]
-      deploy_base = Mix.Project.config[:deploy_base] || "/opt"
-      deploy_dir = Mix.Project.config[:deploy_dir] || Path.join(deploy_base, app_name)
-      tarball = Path.join(["..", "..", "_build", to_string(Mix.env), "rel",
-                           app_name, "releases", version, "#{app_name}.tar.gz"])
+      app_name = Mix.Project.config()[:app] |> Atom.to_string()
+      version = Mix.Project.config()[:version]
+      deploy_base = Mix.Project.config()[:deploy_base] || "/opt"
+      deploy_dir = Mix.Project.config()[:deploy_dir] || Path.join(deploy_base, app_name)
+
+      tarball =
+        Path.join([
+          "..",
+          "..",
+          "_build",
+          to_string(Mix.env()),
+          "rel",
+          app_name,
+          "releases",
+          version,
+          "#{app_name}.tar.gz"
+        ])
 
       defaults = %{
         app_name: app_name,
         deploy_base: deploy_base,
         deploy_dir: deploy_dir,
-        tarball: tarball
+        tarball: tarball,
+        new: false,
+        version: version,
+        upgrade: false
       }
 
-      Enum.reduce args, defaults, fn arg, config ->
+      Enum.reduce(args, defaults, fn arg, config ->
         case arg do
           {:verbosity, verbosity} ->
             %{config | :verbosity => String.to_atom(verbosity)}
+
           {key, value} ->
             Map.put(config, key, value)
         end
-      end
+      end)
     end
   end
-
 end
 
 defmodule Mix.Tasks.Deploy.Local.Rollback do
@@ -115,28 +142,29 @@ defmodule Mix.Tasks.Deploy.Local.Rollback do
 
   def run(args) do
     config = Mix.Tasks.Deploy.Local.parse_args(args)
-    dirs = config.release_base |> File.ls! |> Enum.sort |> Enum.reverse
+    dirs = config.release_base |> File.ls!() |> Enum.sort() |> Enum.reverse()
     rollback(dirs, config)
   end
 
   def rollback([_current, prev | _rest], config) do
     release_dir = Path.join(config.release_base, prev)
     remove_current_link(config)
-    IO.puts "Making link from #{release_dir} to #{config.current_link}"
+    IO.puts("Making link from #{release_dir} to #{config.current_link}")
     File.ln_s(release_dir, config.current_link)
   end
+
   def rollback(dirs, _config) do
-    IO.puts "Nothing to roll back to: releases = #{inspect dirs}"
+    IO.puts("Nothing to roll back to: releases = #{inspect(dirs)}")
   end
 
   def remove_current_link(config) do
     case File.read_link(config.current_link) do
       {:ok, target} ->
-        IO.puts "Removing link from #{target} to #{config.current_link}"
+        IO.puts("Removing link from #{target} to #{config.current_link}")
         :ok = File.rm(config.current_link)
+
       {:error, _reason} ->
-        IO.puts "No current link #{config.current_link}"
+        IO.puts("No current link #{config.current_link}")
     end
   end
-
 end
