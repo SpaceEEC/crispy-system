c741e026475be1b1cf6ded0fc2afa124605553e6
SpaceEEC <spaceeec@yahoo.com>
2018-07-17 20:03:53 +0200

    fix(Image): correct order of function clauses

--- apps/bot_handler/lib/bot_handler/command/image/image.ex
+++ apps/bot_handler/lib/bot_handler/command/image/image.ex
@@ -6,17 +6,17 @@ defmodule Bot.Handler.Command.Image do
 
   def aliases(), do: ["picture"]
 
-  def process(message, args) when length(args) <= 2 do
-    Command.run(Image.Konachan, message, args)
-
-    nil
-  end
-
   def process(message, args) when length(args) <= 5 do
     case :rand.uniform(2) do
       1 -> Command.run(Image.Donmai, message, args)
       2 -> Command.run(Image.Konachan, message, args)
     end
+
+    nil
+  end
+
+  def process(message, args) when length(args) <= 2 do
+    Command.run(Image.Konachan, message, args)
 
     nil
   end
